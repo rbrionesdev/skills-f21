2c25eb1282003542cc34806efb7b8b3267c6b17a
James Yeates <jimibue@gmail.com>
2021-09-15 12:52:36 -0600

    yo

--- app/controllers/api/grades_controller.rb
+++ app/controllers/api/grades_controller.rb
@@ -1,2 +1,22 @@
 class Api::GradesController < ApplicationController
+
+  def index
+    grades = Grade.all_and_then_some
+    render json: grades
+  end
+
+  def create
+    # we have to have the user_id and skill_id
+    @grade = Grade.new(grade_params)
+    if(@grade.save)
+      render json: @grade
+    else
+      render json: {error: @grade.errors}, status: 422
+    end
+  end
+
+  private
+  def grade_params
+    params.require(:grade).permit(:score, :user_id, :skill_id)
+  end
 end
